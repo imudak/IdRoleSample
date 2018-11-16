67df313378e70ecfc8bedc48979ad680e22a5c1c
imudak <imudak@gmail.com>
2018-11-16 13:38:32 +0900

    10. 起動時にAdministrator Roleを生成。

--- IdRoleSample/Program.cs
+++ IdRoleSample/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace IdRoleSample
@@ -14,7 +16,36 @@ namespace IdRoleSample
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var role = "Administrator";
+
+                try
+                {
+                    Task<IdentityResult> roleResult;
+
+                    Task<bool> hasRole = roleManager.RoleExistsAsync(role);
+                    hasRole.Wait();
+
+                    if (!hasRole.Result)
+                    {
+                        roleResult = roleManager.CreateAsync(new IdentityRole(role));
+                        roleResult.Wait();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating Role.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
