e63af208389f76d341276231706c6f19e9fc84f9
imudak <imudak@gmail.com>
2018-11-16 13:12:10 +0900

    2. ID スキャフォールディング Login, Logout, AccessDenied, Registerを実装。

---
+++ IdRoleSample/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IdRoleSample.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdRoleSample.Areas.Identity.IdentityHostingStartup))]
+namespace IdRoleSample.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdRoleSampleContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdRoleSampleContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<IdRoleSampleContext>();
+            });
+        }
+    }
+}
